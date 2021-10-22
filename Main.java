0103e5f56006f0c3b5f28dc399dfb32559c5f48c
Khairun Nisa Manurung <91587041+Cakmoon21@users.noreply.github.com>
2021-10-22 12:19:59 +0700

    java-challenge-5

---
+++ Main.java
@@ -0,0 +1,33 @@
+class Main{ 
+    public static void main(String[] args) {
+      Herois hero = new Herois();
+  
+      hero.setProfile("Cakmoon", 21, 165);
+      hero.profile();
+
+      //jalankan aktivitas disisni...
+      hero.jalan();
+      hero.jalan();
+      hero.lari();
+      hero.lari();
+      hero.lari();
+      hero.lari();
+      hero.makan();
+      hero.makan();
+      hero.minum();
+      hero.minum();
+      hero.lompat();
+      hero.lompat();
+      hero.lompat();
+      hero.lompat();
+      hero.lompat();
+      hero.lompat();
+      hero.lompat();
+      hero.duduk();
+      hero.duduk();
+      hero.duduk();
+      hero.duduk();
+      hero.profile();
+
+    }
+}
